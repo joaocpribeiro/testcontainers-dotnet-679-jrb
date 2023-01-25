28afeba6a4d1847e99c4468e2b6821fc4c483190
Joao Ribeiro <joao.ribeiro@appsfactory.de>
2023-01-25 11:43:06 +0000

    [CHANGE] docker-compose.yml: remove env variable, which is there for docker desktop only.

--- Issue.Tests/GitHub.cs
+++ Issue.Tests/GitHub.cs
@@ -1,38 +1,38 @@
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Containers;
-using System.Threading.Tasks;
-using Xunit;
-using Xunit.Abstractions;
+//using DotNet.Testcontainers.Builders;
+//using DotNet.Testcontainers.Containers;
+//using System.Threading.Tasks;
+//using Xunit;
+//using Xunit.Abstractions;
 
-public sealed class GitHub : IAsyncLifetime
-{
-    private readonly IDockerContainer _container = new TestcontainersBuilder<TestcontainersContainer>()
-        .WithImage("alpine")
-        .WithEntrypoint("top")
-        //.WithDockerEndpoint("unix:///var/run/docker.sock")
-        .Build();
+//public sealed class GitHub : IAsyncLifetime
+//{
+//    private readonly IDockerContainer _container = new TestcontainersBuilder<TestcontainersContainer>()
+//        .WithImage("alpine")
+//        .WithEntrypoint("top")
+//        //.WithDockerEndpoint("unix:///var/run/docker.sock")
+//        .Build();
 
-    private readonly ITestOutputHelper _testOutputHelper;
+//    private readonly ITestOutputHelper _testOutputHelper;
 
-    public GitHub(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
+//    public GitHub(ITestOutputHelper testOutputHelper)
+//    {
+//        _testOutputHelper = testOutputHelper;
+//    }
 
-    public Task InitializeAsync()
-    {
-        return _container.StartAsync();
-    }
+//    public Task InitializeAsync()
+//    {
+//        return _container.StartAsync();
+//    }
 
-    public Task DisposeAsync()
-    {
-        return _container.DisposeAsync().AsTask();
-    }
+//    public Task DisposeAsync()
+//    {
+//        return _container.DisposeAsync().AsTask();
+//    }
 
-    [Fact]
-    public Task GetHostname()
-    {
-        _testOutputHelper.WriteLine(_container.Hostname);
-        return Task.CompletedTask;
-    }
-}
+//    [Fact]
+//    public Task GetHostname()
+//    {
+//        _testOutputHelper.WriteLine(_container.Hostname);
+//        return Task.CompletedTask;
+//    }
+//}
